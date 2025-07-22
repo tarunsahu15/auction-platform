3ab62a209d62dfb91586c825d403a566237d8412
tarunsahu15 <148942846+tarunsahu15@users.noreply.github.com>
2025-07-22 15:19:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ config({
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://helpful-sable-b1a0fe.netlify.app/',
     methods: ["POST", "GET", "PUT", "DELETE"], // give permission which origin can access the server
     credentials: true,
   })
